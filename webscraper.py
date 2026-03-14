155bbe9187aa5ed1884bd1bb00eb1cea33846d86
Yuri966 <yuricm089@gmail.com>
2026-03-14 09:05:40 -0300

    funcionalidade de requests e acesso ao wikipedia

--- webscraper.py
+++ webscraper.py
@@ -1,13 +1,43 @@
 import re
 import requests
 
-def ehValido (url: str):
-    return (re.search("pt.wikipedia.org", url))
+def ehValido (url: str): # se não tiver isso, retorna None
+    return (re.search("pt.wikipedia.org.", url))
+
+def tryResponse (response):
+    if response.status_code == 200:
+        print("Sucesso!")
+    elif response.status_code == 404:
+        print("Não encontrado.")
+    else:
+        print(f"Erro: {response.status_code}")
 
 
+def getHTML (url: str): # pega o html da url
+    response = requests.get(url, headers= cabecalhos)
+    tryResponse(response)
+    html = response.text
+    return html
+
+def getInfo (url: str):
+    padrao_toc = r'<span>(.*)</span>'
+
+    html = getHTML(url)
+    print(re.findall(padrao_toc, html))
+
+
+
+cabecalhos = {
+    'User-Agent': 'WebScraper/1.0 (emailexemplo@gmail.com)'
+}
 
 
 url = input(str("Digite a URL da página do wikipedia: "))
+# re.sub('\s+$', "", url) # retira todos os espaços no final
 
-print(ehValido(url))
+if (ehValido(url) == None):
+    exit()
+
+getInfo(url)
+
 
