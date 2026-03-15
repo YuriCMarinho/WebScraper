b60046f3e7a307561b83d794d085e6880bfd791d
lili1810 <cavalcante.annalivia@gmail.com>
2026-03-15 16:01:21 -0300

    resolve merge conflict

--- webscraper.py
+++ webscraper.py
@@ -6,10 +6,10 @@ cabecalhos = {
 }
 
 
-def ehValido(url: str):  # se não tiver isso, retorna None
+def ehValido(url: str):  # Verifica se a URL pertence à Wikipédia em português
     return re.search("pt.wikipedia.org.", url)
 
-def getHTML(url: str):  # pega o html da url
+def getHTML(url: str):  # pega o html da url e usa 'cabecalhos' para se identificar para o servidor
     response = requests.get(url, headers=cabecalhos, timeout=15)
     if response.status_code != 200:
         raise Exception(f"Erro HTTP {response.status_code}")
@@ -18,12 +18,12 @@ def getHTML(url: str):  # pega o html da url
 
 
 def getInfo(url: str):
-    padrao_toc = r'<span class="vector-toc-numb">[0-9.]+</span>\s*<span>([^<]+)</span>'
-    padrao_img = r'<a href="/wiki/Ficheiro:([^"]+)"[^>]*class="mw-file-description"'
+    padrao_toc = r'<span class="vector-toc-numb">[0-9.]+</span>\s*<span>([^<]+)</span>' # busca números e o texto dentro dos spans do índice
+    padrao_img = r'<a href="/wiki/Ficheiro:([^"]+)"[^>]*class="mw-file-description"' # busca os links de imagem
 
-    artigo_atual = url.split("/wiki/")[1]  # pega o que vem depois de wiki
-    padrao_link = rf'<a href="/wiki/(?!{artigo_atual}")([^":#]+)"'  # pega todos os links que tem comeco = wiki(links internos)
-    # retira todos os links com ":" , "#", e artigos que se referenciam
+    artigo_atual = url.split("/wiki/") [1]                        # pega o que vem depois de wiki
+    padrao_link = rf'<a href="/wiki/(?!{artigo_atual}")([^":#]+)"'# busca todos os links que tem comeco = wiki(links internos)
+                                                                  # retira todos os links com ":" , "#", e artigos que se referenciam
 
     html = getHTML(url)
     toc = re.findall(padrao_toc, html)
@@ -37,9 +37,7 @@ def getInfo(url: str):
         "links": links,
     }
 
-
-# Só roda no terminal se executar diretamente (python webscraper.py)
-if __name__ == "__main__":
+if __name__ == "__main__": # Só roda no terminal se executar diretamente (python webscraper.py)
     url = input(str("Digite a URL da página do wikipedia: "))
     url_limpa = re.sub(r'\s+', '', url)  # retira todos os espaços no final
 
